67a0220e406996d2d38a9fd04997c53d6fa920e9
Jtjenee13 <70092208+Jtjenee13@users.noreply.github.com>
2020-08-23 16:00:29 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,13 @@
+package Setget;
+
+public class Start {
+
+	public static void main(String[] args) {
+		Employee ob1=new Employee();
+ob1.setmilk(10);
+ob1.setwhite(13);
+ob1.setblue(46);
+ob1.show();
+	}
+
+}
